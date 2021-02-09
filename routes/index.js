26a6eac2fa1b52d246900d96f496aff1b4421684
Silviya Velani <silviya@fourplaysocial.com>
2021-02-09 18:58:07 -0500

    Routes added, bootstrap, jquery, fontawesome added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', h1: 'Main Title' });
+  res.render('index', { title: 'Home'});
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me'});
+});
+
+/* GET products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products'});
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services'});
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact me'});
 });
 
 module.exports = router;
